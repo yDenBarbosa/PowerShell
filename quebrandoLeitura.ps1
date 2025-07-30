e0deafc915f17648849d8fce328dccf6e894da8b
yDenBarbosa <denis9195@hotmail.com>
2025-07-29 23:53:18 -0300

    feat:comentarios

--- quebrandoLeitura.ps1
+++ quebrandoLeitura.ps1
@@ -1,12 +1,11 @@
+# Exibe o conteúdo da variável de ambiente Path, que contém os diretórios onde o sistema procura executáveis
 $env:Path
 
-
+# Exibe o tipo de dado da variável Path (geralmente System.String)
 $env:Path.GetType()
 
-
+# Divide o conteúdo da variável Path em partes usando o ponto e vírgula como separador (cada diretório vira um item)
 $env:Path.Split(';')
 
-
+# Lista todos os membros (métodos, propriedades) disponíveis para o conteúdo da variável Path
 $env:Path | Get-Member
-
-
--- resolvedoPowerS.ps1
+++ resolvedoPowerS.ps1
@@ -1,11 +1,18 @@
+# Obtém o serviço especificado em um computador remoto
 $serviço = Get-Service -Name "nome do serviço" -ComputerName "nome do computador/rede"
 
+# Exibe o status atual do serviço (Running, Stopped, etc.)
 $serviço.Status
 
-if ($serviço.Status -eq "Runnig") {
+# Verifica se o serviço está em execução
+if ($serviço.Status -eq "Running") {
+    # Para o serviço
     $serviço.Stop()
+    
+    # Inicia o serviço novamente (reinício)
     $serviço.Start()
 }
 else {
+    # Mensagem exibida se o serviço não estava em execução
     Write-Host "Serviço não estava executando!"
 }
