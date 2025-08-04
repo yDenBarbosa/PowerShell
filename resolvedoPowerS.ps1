cd46fdfcdd2ec5a23ee723ba3195a3d96bacbc9e
yDenBarbosa <denis9195@hotmail.com>
2025-08-03 22:35:00 -0300

    feat:alias

--- resolvedoPowerS.ps1
+++ resolvedoPowerS.ps1
@@ -16,3 +16,14 @@ else {
     # Mensagem exibida se o serviço não estava em execução
     Write-Host "Serviço não estava executando!"
 }
+
+# Comando para obtermos o serviço no computador da rede chamado SRV01
+# DICA: Para obtermos um serviço em nossa máquina local, podemos omitir o argumento -ComputerName
+Get-Service -Name "ByteBank Interface Service" -ComputerName "SRV01"
+
+# Armazenando o resultado do comando em uma variável
+$servico = Get-Service -Name "ByteBank Interface Service" -ComputerName "SRV01"
+
+# Invocando métodos deste objeto
+$servico.Stop()
+$servico.Start()
